687b40f9d99e5c3359b7ba5698cab084bf56f1da
Ankita <ankita@Cosentuss-MacBook-Air.local>
2019-07-14 20:31:53 +0530

    add details entering page

--- app.js
+++ app.js
@@ -16,12 +16,31 @@ mongoose.connect("mongodb://localhost:27017/bookDB", { useNewUrlParser: true });
 
 const personSchema=new mongoose.Schema({
   username:String,
-  password: String
+  password: String,
+  name: String
 });
-//
-// personSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const Person= mongoose.model("Person",personSchema);
+
+const bookSchema=new mongoose.Schema({
+  bookname: String,
+  author:String,
+  chapters: Number,
+  username: String
+});
+
+const Book= mongoose.model("Book", bookSchema);
+
+const reviewSchema= new mongoose.Schema({
+  username: String,
+  bookname: String,
+  chapterno: Number, //wary about you
+  thoughts: String
+});
+
+const Review= mongoose.model("Review", reviewSchema);
+
+var usname="";
 
 app.get("/",function(req,res){
   res.render('index', {filler: ""});
@@ -31,19 +50,35 @@ app.get("/register", function(req,res){
   res.render('register', {filler: ""});
 });
 
-app.post("/",function(req,res){
-  //console.log(req.body);
+app.post("/account", function(req,res){
   Person.findOne({username:req.body.username}, function(err,person){
     if(err){
       console.log(err);
     }
     else{
       if(person===null){
+        //wrong username
         res.render('index', {filler: "This user does not exist. Try again."});
       }
       else{
         if(person.password===req.body.password){
-          res.send("Success");
+          //when match
+          Book.find({username:req.body.username}, function(err,books){
+            if(err){
+              console.log(err);
+            }
+            else{
+              if(books.length===0){
+                //if no books added yet
+                res.render('homepage',{hname:person.name, flag:true});
+
+              }
+              else{
+                //if books added already
+                console.log(books);
+              }
+            }
+          });
         }
         else{
           res.render('index', {filler: "This user does not exist. Try again. Password problem"});
@@ -53,10 +88,15 @@ app.post("/",function(req,res){
   });
 });
 
+app.get("/homepage", function(req,res){
+
+});
+
 app.post("/register",function(req,res){
   const person=new Person({
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
+    name: req.body.name
   });
   Person.find({username:req.body.username},function(err,persons){
     if(err){
@@ -65,12 +105,21 @@ app.post("/register",function(req,res){
     else{
       if(persons.length===0){
         person.save();
+        res.render('index', {filler: "Login with your credentials now!"});
       }
       else{
         res.render('register', {filler: "Username taken. Please try again."});
       }
     }
   });
+});
+
+app.post("/add",function(req,res){
+  res.render('addbook');
+});
+
+app.post("/added", function(req,res){
+
 });
 
 app.listen(3000, function() {
