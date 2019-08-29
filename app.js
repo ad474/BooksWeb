776353aedc785c651cbcfd17b9d665a335c519be
Ankita <ankita@Cosentuss-MacBook-Air.local>
2019-08-30 00:47:38 +0530

    commit before deployment v2

--- app.js
+++ app.js
@@ -485,6 +485,11 @@ app.post("/logout", function(req,res){
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+let port=process.env.PORT;
+if(port==null||port==""){
+  port=3000;
+}
+
+app.listen(port, function() {
   console.log("Server has started successfully");
 });
